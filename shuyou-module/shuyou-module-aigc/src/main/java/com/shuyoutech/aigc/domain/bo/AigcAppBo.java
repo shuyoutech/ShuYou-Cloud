f93bef53f1592de7e79873f0c65b13e6c6cbeccd
YangChao <1219191915@qq.com>
2025-08-18 12:01:52 +0800

    add: init project

---
+++ shuyou-module/shuyou-module-aigc/src/main/java/com/shuyoutech/aigc/domain/bo/AigcAppBo.java
@@ -0,0 +1,47 @@
+package com.shuyoutech.aigc.domain.bo;
+
+import com.shuyoutech.aigc.domain.entity.AigcAppEntity;
+import com.shuyoutech.common.core.model.group.StatusGroup;
+import com.shuyoutech.common.core.model.group.UpdateGroup;
+import io.github.linpeilie.annotations.AutoMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author YangChao
+ * @date 2025-07-12 09:29:09
+ **/
+@Data
+@AutoMapper(target = AigcAppEntity.class)
+@Schema(description = "应用类")
+public class AigcAppBo implements Serializable {
+
+    @NotBlank(message = "id不能为空", groups = {UpdateGroup.class, StatusGroup.class})
+    @Schema(description = "主键")
+    private String id;
+
+    @Schema(description = "应用类型")
+    private String appType;
+
+    @Schema(description = "应用名称")
+    private String appName;
+
+    @Schema(description = "应用图标")
+    private String appIcon;
+
+    @Schema(description = "应用描述")
+    private String appDesc;
+
+    @Schema(description = "对话模型ID")
+    private String chatModelId;
+
+    @Schema(description = "知识库ID")
+    private String knowledgeId;
+
+    @Schema(description = "提示词")
+    private String promptText;
+
+}
